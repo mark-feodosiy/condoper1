18a45f0eaa9ab5765ab77282e1f5a25039fd191a
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-10-14 15:24:46 +0300

    Домашка с условными операторами

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    int age = 18;
+    if (age >= 18) {
+        System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний "}
+    }
+    else {
+        System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+    }
+
+
+
 }
